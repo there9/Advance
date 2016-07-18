99a8be987615cbbf3a094616600691933671de41
Jeonjinhee <pretty9922@naver.com>
2016-07-18 12:35:11 +0900

    Revert "진희꺼"
    This reverts commit 686b311c211463a70c80e1b48ba95a16da27fced.

--- timetable/timetable.infra/Event/Event.cs
+++ timetable/timetable.infra/Event/Event.cs
@@ -23,8 +23,6 @@ namespace TimeTable.Infra
     public class TimeTableConflictEvent : PubSubEvent<object> { }
     //SelectTiemTable 하나 삽입 버튼 또는 더블클릭 눌렀을 때의 이벤트 (pub SelectTimeTable)(Sub ShowTimeTable)
     public class RequestAddItemEvent : PubSubEvent<object> { }
-    
-    public class LoadImgEvent : PubSubEvent<object> { }
 
 
 }
